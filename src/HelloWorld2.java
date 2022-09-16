b931eb35c605c3437af18a52725491b30518ac54
Bianca <s52340@edu.campus02.at>
2022-09-16 19:11:27 +0200

    my first commit

---
+++ src/HelloWorld2.java
@@ -0,0 +1,13 @@
+public class HelloWorld2 {
+    //main oder psvm
+    public static void main(String[] args) {
+        // sout
+
+
+        System.out.println("Hello World 2");
+        System.out.println("Welcome");
+        System.out.println("Zeile eins");
+        System.out.println("Zeile zwei");
+        System.out.println("Zeile drei");
+    }
+}
